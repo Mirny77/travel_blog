424ce0f517ba0c469ba1ad58af5c4684a75ab16f
Mirny77 <mironov.ilya20160@yandex.ru>
2020-05-11 16:17:04 +0300

    update

---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.layout')
+@section('content')
+  <form action="{{route('post.store')}}" method="post" enctype="multipart/form-data">@csrf
+      <h2>Создать пост</h2>
+      <div class="form-group">
+          <input type="text" class="form-control" name="title" required>
+      </div>
+      <div class="form-group">
+          <textarea name="descr" rows ="10" class="form-control" required></textarea>
+      </div>
+      <div class="form-group">
+          <input type = "file" name="img">
+      </div>
+
+      <input type="submit" value="Создать пост" class="btn-outline-primary">
+  </form>
+
+@endsection
+
---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.layout')
+@section('content')
+    @if(isset($_GET['search']))
+        @if(count($posts)>0)
+            <h2>Результат по запросу "<?= $_GET['search'] ?>"</h2>
+            <p class="'lead">
+                Найдено {{count($posts)}} постов
+            </p>
+        @else
+            <h2>Результат по запросу "<?= $_GET['search'] ?>" ничего не дал</h2>
+            <a href="{{route('post.index')}}" class="btn btn-outline-primary">Вернуться на главную</a>
+
+        @endif
+        @endif
+    <div class="row">
+        @foreach($posts as $post)
+        <div class="col-6">
+            <div class="card">
+                <div class="card-header">{{$post->short_title}}</div>
+                <div class="card-body">
+                    <div class="card-img" style="background-image: url({{$post->img ?? asset("img/22.png")}}) "></div>
+                    <div class="card-author">Создатель: {{$post->name}}</div>
+                    <a href="#" class="btn btn-outline-primary">Посмотреть</a>
+                </div>
+
+
+            </div>
+        </div>
+        @endforeach
+    </div>
+    @if(!isset($_GET['search']))
+    {{$posts->links()}}
+    @endif
+
+@endsection
